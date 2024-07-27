3478c1b41f81353d5cef804dc076ddf27cc46578
Pelumi Balogun <pjohnsonPJB@gmail.com>
2024-07-27 09:52:17 +0100

    Intial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Anime-Plum
+
+Anime-Plum is a simple project to explore the functionality of server actions in Next.js. It provides information on popular anime and their ratings, utilizing animations and intersection observer techniques.
+
+## Features
+
+- Display popular anime with ratings
+- Smooth animations using Framer Motion
+- Lazy loading of anime details with React Observe Intersection
+
+## Libraries Used
+
+- [Next.js](https://nextjs.org/)
+- [Framer Motion](https://www.framer.com/motion/)
+- [React Observe Intersection](https://www.npmjs.com/package/react-observe-intersection)
+
+## Tutorial Followed
+
+This project is based on a tutorial from [JavaScript Mastery](https://www.youtube.com/c/JavaScriptMastery).
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/anime-plum.git
+
+2. Navigate to the project directory:
+
+    cd anime-plum
+
+
+3. Install dependencies:
+
+   npm install
+
+## Usage
+Start the development server:
+    npm run dev
---
+++ components/LoadMore.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { fetchAnime } from "@/app/action";
+import Image from "next/image";
+import { useEffect } from "react";
+import {useInView}   from  "react-intersection-observer";
+import { useState } from "react";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+
+function LoadMore() {
+  const {ref, inView} = useInView();
+  const [data, setData] = useState<AnimeProp[]>([])
+
+
+  let page = 2
+
+  useEffect(()=>{
+     if(inView){
+      console.log("Loading more...");
+      fetchAnime(2).then((res)=>{
+          setData(prevData => [...prevData, ...res]);
+      })
+      page++;
+     }
+  },[inView])
+  return (
+    <>
+    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+      {
+        data.map((item: AnimeProp, index:number)=> (
+          <AnimeCard key={item.id} anime={item} index={index}/>
+        ))
+
+      }
+    </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref ={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
